8c81e1d1b7e8cd28254cab6b869ca009d703fe0e
Ancymariam Joseph <ancymariam.joseph@gmail.com>
2015-03-21 14:49:47 -0700

    first Commit check to ancyjoseph/ProgAssignment account

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+ ##Initialize inverse matrix to null 
+  m <- NULL
+  
+  ## Set matrix fuction for input matrix
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  ## Get function for input matrix
+  get <- function() x
+  
+  ## Set the inverse matrix
+  setinverse <- function(inverse) m <<- inverse
+  
+  ## Get the inverse matrix
+  getinverse <- function() m
+  
+  ## List to hold the set and get function values of the input matrix and inverse matrix
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
